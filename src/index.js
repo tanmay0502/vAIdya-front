c56e9d0a1873af68232d9110b2ffee568c17d779
tanmay0502 <81376006+tanmay0502@users.noreply.github.com>
2022-03-30 11:33:53 +0530

    hover-border-added

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import InName from './components/inName';
 
 ReactDOM.render(
   
-  <div className='flex flex-col flex-auto justify-center items-center bg-black h-screen'>
-  <div className="bg-red-100 bg-opacity-10 p-20">
+  <div className=' flex flex-col flex-auto justify-center items-center bg-black h-screen'>
+  <div className=" hover:border-2 border-amber-900 bg-red-100 bg-opacity-10 p-20">
     <Hello></Hello>
     <InName></InName>
     <Button></Button>
